f12c3849f0709a221ab5a9e049ae4a09ab71fbc5
Mike Pronin <mr.mi@mail.ru>
2023-03-13 23:14:00 +0400

    задача 66

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -2,3 +2,23 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// Печатаем сумму натуральных элементов в промежутке от M до N.
+/// </summary>
+/// <param name="numberM">M = start</param>
+/// <param name="numberN">N = end</param>
+/// <returns></returns>
+int SumNumbers(int numberM, int numberN)
+{
+  //Базовый
+  if (numberM == numberN) return numberM;
+  //Рекурсивный
+  return (numberM + SumNumbers(numberM + 1, numberN));
+}
+
+Console.WriteLine(SumNumbers(M, N));
